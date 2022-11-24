880a51ed406718d64a68fb8b8e50e9df33d28ddd
nestorap <nalaminospelegrina@gmail.com>
2022-11-24 12:39:18 +0100

    ya de ya

--- utilities/setter.py
+++ utilities/setter.py
@@ -7,6 +7,7 @@ from base64 import b64decode
 #
 def main():
     key = os.environ.get("DRIVE_SECRET")
+    key = str(key)
     with open("path.json", "w") as json_file:
         json_file.write(b64decode(key).decode())
     print(os.path.realpath(path.json))
