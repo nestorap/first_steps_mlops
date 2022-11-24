d0848c4ab1895c4d265d0d43040e2e6b4bddd7fe
nestorap <nalaminospelegrina@gmail.com>
2022-11-24 12:28:36 +0100

    arreglando fallos

--- utilities/setter.py
+++ utilities/setter.py
@@ -4,7 +4,7 @@ Con este script vamos a poder leer las credentials de google
 
 import os
 from base64 import b64decode
-
+#
 def main():
     key = os.environ.get("DRIVE_SECRET")
     with open("path.json", "w") as json_file:
