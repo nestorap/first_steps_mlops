7692170b46f4637fdc016058ca434063c6a2cac7
nestorap <nalaminospelegrina@gmail.com>
2022-11-24 12:08:14 +0100

    probando secret en sstring

--- utilities/setter.py
+++ utilities/setter.py
@@ -8,8 +8,7 @@ from base64 import b64decode
 def main():
     key = os.environ.get("DRIVE_SECRET")
     with open("path.json", "w") as json_file:
-        json = json_file.write(b64decode(key).decode())
-        str(json)
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath(path.json))
 
 if __name__ == "main":
