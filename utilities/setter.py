a530ba6703452236f8a94434620ed7c5d317d63b
nestorap <nalaminospelegrina@gmail.com>
2022-11-24 12:00:37 +0100

    a ver si ya

--- utilities/setter.py
+++ utilities/setter.py
@@ -8,7 +8,8 @@ from base64 import b64decode
 def main():
     key = os.environ.get("DRIVE_SECRET")
     with open("path.json", "w") as json_file:
-        json_file.write(str(b64decode(key).decode()))
+        json = json_file.write(b64decode(key).decode())
+        str(json)
     print(os.path.realpath(path.json))
 
 if __name__ == "main":
